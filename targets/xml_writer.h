3d9630d16223ef8f4fa260b353f7fa719e9b48e0
Luís Calado <luis.maria.calado@tecnico.ulisboa.pt>
2024-05-14 14:43:08 +0100

    fix: changed types back to const char*

--- targets/xml_writer.h
+++ targets/xml_writer.h
@@ -64,10 +64,10 @@ namespace til {
   protected:
     void do_binary_operation(cdk::binary_operation_node *const node, int lvl);
     void do_unary_operation(cdk::unary_operation_node *const node, int lvl);
-    inline std::string bool_to_str(bool boolean) {
+    inline const char *bool_to_str(bool boolean) {
       return boolean ? "true" : "false";
     }
-    inline std::string qualifier_to_str(int qualifier) {
+    inline const char *qualifier_to_str(int qualifier) {
       switch (qualifier) {
         case tEXTERNAL: return "external";
         case tFORWARD: return "forward";
